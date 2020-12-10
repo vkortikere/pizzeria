5616cc9fdc90135d106bad118577d9076782e2d8
vkortikere <69329819+vkortikere@users.noreply.github.com>
2020-12-09 23:15:54 -0500

    Pizzaria-C-79
    Pizzaria-C-79

---
+++ main.js
@@ -0,0 +1,31 @@
+var menu_list_array = ["Garden Fresh Pizza","Cheeese Pizza","Pineapple Pizza",
+                       "Spinach-Olive Pizza","Broccoli Pizza","Eggplant Parmesian Pizza"
+                       ];
+function getmenu(){
+    
+    var htmldata;
+    htmldata="<ol class='menu_list'>"
+    
+    menu_list_array.sort();
+    
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>'+ menu_list_array[i] + '</li>'
+
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class ='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<div class="card">' + '<img src="images/pizza_menu.png"/>' + menu_list_array[i] + '</div>'
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+
+}
